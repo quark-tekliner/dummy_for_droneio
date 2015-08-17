dad55fcbc35e704552e3ae8dfcad83e05d8255ff
quark <quark@xakep.ru>
2015-08-18 03:59:26 +0600

    Add dummy app and test

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+from app import foo, bar
+
+
+class AppTestCase(unittest.TestCase):
+
+  def test_foo(self):
+    self.assertEqual(foo(1, 2), 3)
+
+  def test_bar(self):
+    self.assertEqual(bar(1, 2), -1)
+
+if __name__ == '__main__':
+  unittest.main()
