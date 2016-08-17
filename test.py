9335a2faa038d7287ba61f08d810c2e593f475ce
quark <quark@xakep.ru>
2016-08-18 02:44:16 +0700

    Fix it.

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class AppTestCase(unittest.TestCase):
     self.assertEqual(foo(1, 2), 3)
 
   def test_bar(self):
-    self.assertEqual(bar(1, 2), -3)
+    self.assertEqual(bar(1, 2), -1)
 
 if __name__ == '__main__':
   unittest.main()
